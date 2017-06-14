99d280dfd626d1f2c315e3a078925388bad47829
Samuel Amoah <snamoah@gmail.com>
2017-06-14 13:33:40 +0000

    add font-awesome

--- lib/theme.jsx
+++ lib/theme.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // import nifty theme css
+import './css/font-awesome/font-awesome.min.css';
 import './css/bootstrap.css';
 import './css/nifty.css';
 import './css/custom.css';
